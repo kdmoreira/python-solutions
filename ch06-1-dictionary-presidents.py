c5f4856b2a35e1ce5b2482e5facb52dfb66fb5a7
Karina Moreira <kakatinosa@hotmail.com>
2020-11-27 19:59:17 -0300

    Add file

---
+++ ch06-1-dictionary-presidents.py
@@ -0,0 +1,14 @@
+# 6.1
+def estadoNasc(name):
+    if name in dict:
+        return dict[name]
+    else:
+        print("Name not found.")
+        
+dict = {'Barack Hussein Obama II':'Hawaii',
+        'George Walker Bush':'Connecticut',
+        'William Jefferson Clinton':'Arkansas',
+        'George Herbert Walker Bush':'Massachussetts',
+        'Ronald Wilson Reagan':'Illinois',
+        'James Earl Carter, Jr':'Georgia'}
+
