131ce090283e273e25d4f6f354602d4f43c760f8
Karina Moreira <kakatinosa@hotmail.com>
2020-11-28 15:40:23 -0300

    Add file

---
+++ ch06-4-word-count-function.py
@@ -0,0 +1,31 @@
+# 6.4
+def processText(your_text):
+    """This function processes text and returns a list of words."""
+    new_text = your_text.split()
+    for i in new_text:
+        word = i.strip("\".,;!?()")
+        word = word.casefold()
+        word_list.append(word)
+    return word_list
+
+def countWords(any_list):
+    """This function counts word frequency."""
+    for word in any_list:
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+    return counter
+
+wiki = "Python is dynamically typed and garbage-collected. \
+It supports multiple programming paradigms, including structured \
+(particularly, procedural), object-oriented, and functional programming. \
+Python is often described as a \"batteries included\" language due to \
+its comprehensive standard library."
+
+word_list = []
+counter = {}
+
+processText(wiki)
+countWords(word_list)
+print(counter)
