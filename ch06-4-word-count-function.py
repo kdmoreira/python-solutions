92cbc2d8580e240e8d06edde5581e32c99305dbb
Karina Moreira <kakatinosa@hotmail.com>
2020-12-01 22:10:15 -0300

    Add print function for each loop

--- ch06-4-word-count-function.py
+++ ch06-4-word-count-function.py
@@ -1,6 +1,6 @@
 # 6.4
 def processText(your_text):
-    """This function processes text and returns a list of words."""
+    """This function processes text and returns a list of words"""
     new_text = your_text.split()
     for i in new_text:
         word = i.strip("\".,;!?()")
@@ -9,23 +9,21 @@ def processText(your_text):
     return word_list
 
 def countWords(any_list):
-    """This function counts word frequency."""
+    """This function counts word frequency"""
     for word in any_list:
         if word in counter:
             counter[word] += 1
         else:
             counter[word] = 1
-    return counter
+    for word in counter:
+        print("{} appears {} time(s).".format(word, counter[word]))
 
 wiki = "Python is dynamically typed and garbage-collected. \
 It supports multiple programming paradigms, including structured \
 (particularly, procedural), object-oriented, and functional programming. \
 Python is often described as a \"batteries included\" language due to \
 its comprehensive standard library."
-
 word_list = []
 counter = {}
-
 processText(wiki)
 countWords(word_list)
-print(counter)
